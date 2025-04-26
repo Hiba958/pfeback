b107c8fb355eda1a82036d0764129acde4ad1ab1
Hiba958 <amarhiba010@gmail.com>
2025-04-26 23:19:58 +0100

    Update Device.js

--- models/Device.js
+++ models/Device.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const deviceSchema = mongoose.Schema({
     name: { type: String, required: true },
-    reference: { type: String, required: true }
+    reference: { type: String, required: true },
+    status: {
+        type: String,
+        enum: ['disponible', 'réservé'],
+        default: 'disponible'
+    }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Device', deviceSchema);
