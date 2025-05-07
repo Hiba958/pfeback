204bcbb99ad99f43cfa634a45002be8217c2a885
Hiba958 <amarhiba010@gmail.com>
2025-05-07 16:20:02 +0100

    Update Reservation.js

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 
-const reservationSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  device: { type: mongoose.Schema.Types.ObjectId, ref: 'Device' },
-  status: { type: String, enum: ['en_attente', 'validée', 'refusée'], default: 'en_attente' },
-  createdAt: { type: Date, default: Date.now }
+const reservationSchema = mongoose.Schema({
+    device: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Device',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['en_attente', 'validée', 'refusée'],
+        default: 'en_attente'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-module.exports = mongoose.model('Reservation', reservationSchema);
+module.exports = mongoose.model('Reservation', reservationSchema); 
